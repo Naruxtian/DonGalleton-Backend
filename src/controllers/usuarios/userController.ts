45593ca5e0ca538988065495144c0850b9f91491
Naruxtian <Naruxtian@Hotmail.com>
2023-12-05 17:49:55 -0600

    Log para metrica del login

--- src/controllers/usuarios/userController.ts
+++ src/controllers/usuarios/userController.ts
@@ -44,6 +44,7 @@ export const registerUser = asyncHandler( async (req: Request, res: Response, ne
 
 export const login = asyncHandler( async (req: Request, res: Response, next: NextFunction) => {
     try{
+        const init = Date.now();
         const {email, password} = req.body;
         const q = query(collection(db, "users"));
         const querySnapshot = await getDocs(q);
@@ -51,13 +52,40 @@ export const login = asyncHandler( async (req: Request, res: Response, next: Nex
         querySnapshot.forEach((doc) => {
             usrs.push({ ...doc.data(), id: doc.id });
         });
-
+        
         const user = usrs.find((user) => user.email === email && user.password === password);
+        const finish = Date.now();
+        const rendimiento = finish - init;
+        const fecha = new Date();
+        const fechaActual = fecha.toLocaleDateString();
+        const horaActual = fecha.toLocaleTimeString();
+        const log = {
+            fecha: fechaActual+ ', ' + horaActual,
+            usario: email,
+            logueado: true,
+            rendimiento: rendimiento+'ms',
+            movimiento: 'Login'
+        }
         if (!user) {
+            log.logueado = false;
+            await addDoc(collection(db, "logs"), log);
             return next(new ErrorResponse("El email o la contraseña no son correctos", 400));
         }
+
+        await addDoc(collection(db, "logs"), log);
         new ResponseHttp(res).send("Usuario logueado correctamente", user, true, 200);
     } catch (error: any) {
+        const fecha = new Date();
+        const fechaActual = fecha.toLocaleDateString();
+        const horaActual = fecha.toLocaleTimeString();
+        const log = {
+            fecha: fechaActual+ ', ' + horaActual,
+            usuario: req.body.email,
+            logueado: false,
+            rendimiento: null,
+            movimiento: 'Login'
+        }
+        await addDoc(collection(db, "logs"), log);
         const errorCode = error.code;
         const errorMessage = error.message;
         next(new ErrorResponse(errorMessage, errorCode));
